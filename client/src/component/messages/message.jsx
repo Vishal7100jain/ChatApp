285396f83cc4860aaa9bd0b6e90a9d9a0e64e21b
Vishal <Vishaljain7100@gmail.com>
2024-04-10 22:51:01 +0530

    message div is ready with emoji reaction on it

--- client/src/component/messages/message.jsx
+++ client/src/component/messages/message.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useRef } from 'react'
-import Picker from 'emoji-picker-react'
 
 const message = ({ Message }) => {
-    const time = new Date(Message.createdAt).toLocaleTimeString()
+    const time = new Date(Message.createdAt)
+    const hrs = time.getHours()
+    const mint = time.getMinutes()
+    const AM_PM = time.getTimezoneOffset()
     const user = JSON.parse(localStorage.getItem('User'))?.user
     const { SelectedUserToChat } = useSelector(state => state.user)
     let { ShakeMessage } = useSelector(state => state.user)
     const View = useRef()
     let [ShowReaction, SetReaction] = useState(false)
-    const { PhoneView } = useSelector(state => state.user)
+    let [ShowSelectedEmoji, SetEmojiOnMessage] = useState()
 
     useEffect(() => {
         setTimeout(() => {
@@ -21,6 +23,10 @@ const message = ({ Message }) => {
     const ChatPosition = Message.senderId === user._id
 
     const handleShowReacton = (e) => {
+        SetEmojiOnMessage((pre) => {
+            if (pre != e.target.innerHTML) return pre = e.target.innerHTML
+            else return pre = ""
+        })
         SetReaction(false)
     }
 
@@ -34,24 +40,24 @@ const message = ({ Message }) => {
     ]
 
     return (
-        <div className={`chat ${ChatPosition ? "chat-end" : "chat-start"}`}>
+        <div className={`chat ${ChatPosition ? "chat-end" : "chat-start"} mb-3`}>
             <div className='chat-image avatar'>
                 <div className='w-10 rounded-full'>
                     <img src={ChatPosition ? user.ProfilePic : SelectedUserToChat?.ProfilePic} alt="user avatar" />
                 </div>
             </div>
-            <div className='Grid grid-cols-2'>
-                {ShowReaction && <div className="emojis text-3xl p-2" style={{ backgroundColor: "#374151" }}>
+            <div className="md:w-2/4 xs:w-10/12" style={{ display: 'grid', justifyContent: `${ChatPosition ? "end" : ""}` }} >
+                {ShowReaction && <div className={`emojis text-3xl p-2 flex rounded-full text-center`} style={{ width: "18rem", backgroundColor: "#374151" }}>
                     {emojis.map((em) => {
                         return <span onClick={(e) => handleShowReacton(e)} className='hover:text-4xl cursor-pointer'>{em.emoji}</span>
                     })}
                 </div>}
-                <div style={ChatPosition ? { justifyContent: "end", display: 'grid', position: 'relative' } : null}>
-                    <div onClick={() => SetReaction((pre => !pre))} className={`chat-bubble flex text-white bg-blue-500  ${!ChatPosition && ShakeMessage ? "message-shake" : ""} `} ref={View}>
+                <div className='w-full' style={ChatPosition ? { display: "grid", justifyContent: "end", position: 'relative' } : { display: 'grid', position: "relative", justifyContent: "start" }}>
+                    <div onClick={() => SetReaction((pre => !pre))} className={`mb-3 max-w-none chat-bubble flex w-full text-white bg-blue-500  ${!ChatPosition && ShakeMessage ? "message-shake" : ""} `} ref={View}>
                         {Message.message}
-                        <span className='absolute bottom-0 top-10 left-0 rounded-full' style={{ backgroundColor: "#374151" }}>🙏</span>
+                        <span className={`rounded-full text-2xl ${!ChatPosition ? 'right-0' : 'left-0'}`} style={{ position: 'absolute', top: '55%' }}>{ShowSelectedEmoji}</span>
                     </div>
-                    <div className={`chat-fotter opacity-50 text-xs flex gap-1 items-center`}>{time}</div>
+                    <div className={` absolute ${ChatPosition ? 'right-0' : ''} chat-fotter opacity-50 text-xs text-start flex gap-1 items-center`} style={{ fontSize: "0.68rem", top: "89%", width: "3rem" }}>{hrs}:{mint} {hrs >= 12 ? "PM" : "AM"}</div>
                 </div>
             </div >
         </div >
