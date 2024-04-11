2e1276757129f5e516e97268a01d1fcf59b0656d
Jain Vishal DineshKumar <134675294+Vishal7100jain@users.noreply.github.com>
2024-04-11 13:17:49 +0530

    Update auth.js

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -26,7 +26,7 @@ export const SignUp = async (req, res) => {
 
 export const Login = async (req, res) => {
     let { username, password } = req.body
-    const user = await User.findOne({ username: username }).populate("Friends").populate("PendingReq").select("-password")
+    const user = await User.findOne({ username: username }).populate("Friends").populate("PendingReq")
     if (!user) return res.status(400).json({ message: "User not found" })
     const clearPassword = await bcrypt.compare(password, user.password)
     if (clearPassword) {
