e7eaa548a426536d87a94b7ae85c1183a13107d9
Vishal <Vishaljain7100@gmail.com>
2024-04-11 13:12:18 +0530

    password error solved

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -9,14 +9,7 @@ export const SignUp = async (req, res) => {
     const user = await User.findOne({ username: username })
     if (user) return res.status(400).json({ message: "Choose a Unique Username" })
 
-    // Checking Acctual Exist
-    const checkPass = await bcrypt.compare(password, password)
-    const userWithAlreadyExistAccount = await User.findOne({ username: username, password: checkPass })
-    if (userWithAlreadyExistAccount) return res.status(400).json({ message: "User already exist" })
-
     const hashPassword = await bcrypt.hash(password, 12)
-
-
     const boyProfilePic = ProfilePic ? ProfilePic : `https://avatar.iran.liara.run/public/boy?username=${username}`
     const girlProfilePic = ProfilePic ? ProfilePic : `https://avatar.iran.liara.run/public/girl?username=${username}`
 
