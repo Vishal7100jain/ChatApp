f89416c46db7473e409e3c5837f5e7729af90ade
Jain Vishal DineshKumar <134675294+Vishal7100jain@users.noreply.github.com>
2024-04-12 08:00:45 +0530

    Update messages.jsx

--- client/src/component/messages/messages.jsx
+++ client/src/component/messages/messages.jsx
@@ -6,7 +6,7 @@ const messages = () => {
     const { Messages } = useSelector(state => state.message)
 
     return (
-        <div className='px-4 w-full flex-1 overflow-auto mb-10 xs:mt-15'>
+        <div className='px-4 w-full flex-1 overflow-auto mb-10 xs:mt-16'>
             {Messages && Messages.map((msg) => {
                 return < Message key={msg._id} Message={msg} />
             })}
